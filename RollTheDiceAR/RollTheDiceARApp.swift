fa9c3507c8b46a0a4fe9c5ca1fae3e019ba4cf20
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-27 22:06:33 +0100

    Initial Commit

---
+++ RollTheDiceAR/RollTheDiceARApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceARApp.swift
+//  RollTheDiceAR
+//
+//  Created by Zaid Neurothrone on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
