743db26b0af76cfa8f4688ff9775b9d8f0bfb991
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-27 22:19:12 +0100

    add ARViewContainer to ContentView body

--- RollTheDiceAR/ContentView.swift
+++ RollTheDiceAR/ContentView.swift
@@ -11,13 +11,10 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello, world!")
+    ZStack {
+      ARViewContainer()
+        .edgesIgnoringSafeArea(.all)
     }
-    .padding()
   }
 }
 
