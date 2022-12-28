a4f85b2f26b3b2709e6038659455a5ab4eb6b551
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-28 21:59:31 +0100

    add temporary about view with credits

---
+++ RollTheDiceAR/Views/AboutView.swift
@@ -0,0 +1,28 @@
+//
+//  AboutView.swift
+//  RollTheDiceAR
+//
+//  Created by Zaid Neurothrone on 2022-12-28.
+//
+
+import SwiftUI
+/*
+ UI Click:
+ Sound Effect from Pixabay
+ https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=43196
+ 
+ Dice Sound: from
+ https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=40081
+ */
+
+struct AboutView: View {
+  var body: some View {
+    Text("Sound Effect from Pixabay")
+  }
+}
+
+struct AboutView_Previews: PreviewProvider {
+  static var previews: some View {
+    AboutView()
+  }
+}
